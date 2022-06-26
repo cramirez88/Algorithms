00fc11b4740b08ed586328d21da531c1a66f6713
Christian Ramirez <christian.r0602@gmail.com>
2022-06-26 15:51:05 -0500

    simpleArraySum

--- script.js
+++ script.js
@@ -44,7 +44,21 @@
 //   return a + b
 // }
 
+// Function Description
 
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+// simpleArraySum has the following parameter(s):
+
+// ar: an array of integers
+
+// function simpleArraySum(ar) {
+//   let sum = 0
+//   for (let i = 0; i < ar.length; i++){
+//       sum += ar[i]
+//   }
+//   return sum
+// }
 
 
 
